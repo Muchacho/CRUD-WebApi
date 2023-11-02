8a8d51dce8a6b6959a77c3d6af85449d28cb092f
Muchacho <0elephant123@gmail.com>
2023-11-02 18:29:42 +0300

    controller and router added

--- controllers/MeetupController.js
+++ controllers/MeetupController.js
@@ -2,5 +2,48 @@ const MeetupService = require('../services/MeetupService');
 const service = new MeetupService();
 
 module.exports = class MeetupController {
-    
+    async getAllMeetups(request, response){
+        try{
+            let result = await service.getAllMeetups();
+            response.end(JSON.stringify(result));
+        } catch(error){
+            next(error, request, response)
+        }
+    }
+
+    async getMeetup(request, response){
+        try{
+            let result = await service.getMeetupById(request.params['name']);
+            response.end(JSON.stringify(result));
+        } catch(error){
+            next(error, request, response)
+        }
+    }
+
+    async addMeetup(request, response, next){
+        try{
+            await service.addMeetup(request.body);
+            response.status(200).end();
+        } catch(error){
+            next(error, request, response)
+        }
+    }
+
+    async updateMeetup(request, response){
+        try{
+            service.updateMeetup(request.params['name'], request.body);
+            response.status(200).end();
+        } catch(error){
+            next(error, request, response)
+        }
+    }
+
+    async deleteMeetup(request, response){
+        try{
+            await service.deleteMeetup(request.params['name'])
+            response.status(200).end();
+        } catch(error){
+            next(error, request, response)
+        }
+    }
 }
--- routers/MeetupRounter.js
+++ routers/MeetupRounter.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const MeetupController = require('../controllers/MeetupController');
 const meetupController = new MeetupController();
 
+router.get('', meetupController.getAllMeetups);
+router.get('/:name', meetupController.getMeetup);
+router.post('/', meetupController.addMeetup);
+router.put('/:name', meetupController.updateMeetup);
+router.delete('/:name', meetupController.deleteMeetup);
+
 
 module.exports = router;
