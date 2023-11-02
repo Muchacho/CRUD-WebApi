227340f8516ae73a847582da666cc0cba069953c
Muchacho <0elephant123@gmail.com>
2023-11-02 17:41:03 +0300

    service and dto created

--- dto/meetup.dto.js
+++ dto/meetup.dto.js
@@ -1,5 +1,9 @@
 module.exports = class MeetupDto{
     constructor(data){
-        
+        this.name = data.name;
+        this.description = data.description;
+        this.time = data.time;
+        this.address = data.address;
+        this.keywords = data.keywords.map(keyword => keyword.word)
     }
 }
--- services/MeetupService.js
+++ services/MeetupService.js
@@ -3,5 +3,91 @@ const prisma = new PrismaClient()
 const MeetupDto = require('../dto/meetup.dto');
 
 module.exports = class MeetupService{
-    
+    async getAllMeetups(){        
+        let meetups = await prisma.meetups.findMany({
+            select: {
+                id: true,
+                name: true,
+                description: true,
+                time: true,
+                address: true,
+                keywords: {
+                    select: {
+                        word: true
+                    }
+                }
+            }
+        });
+        if(!meetups.length) return 'Meetups not found';
+        let meetupsDto = meetups.map((meetup)=> new MeetupDto(meetup));        
+        return meetupsDto;
+    }
+
+    async getMeetupById(name){
+        let meetup = await prisma.meetups.findFirst({
+            where: { name },
+            select: {
+                id: true,
+                name: true,
+                description: true,
+                time: true,
+                address: true,
+                keywords: {
+                    select: {
+                        word: true
+                    }
+                }
+            }
+        });
+        if(!meetup) return `Meetup with id ${name} not found`;
+        let meetupDto = new MeetupDto(meetup);
+        return meetupDto;
+    }
+
+    async addMeetup(data){
+        await prisma.meetups.create({
+            data: {
+                name: data.name,
+                description: data.description,
+                time: data.time,
+                address: data.address,
+                keywords: {
+                    createMany: {
+                        data: data.keywords
+                    }
+                }
+            }
+        });
+    }
+
+    async updateMeetup(name, data){
+        let updMeetup = await prisma.meetups.update({
+            where: {
+                name
+            },
+            data: {
+                name: data.name,
+                description: data.description,
+                time: data.time,
+                address: data.address,
+            }
+        });
+        if(data.keywords){
+            await prisma.keywords.deleteMany({where:{meetupid: updMeetup.id}});
+            for(let item in data.keywords){
+                await prisma.keywords.create({
+                    data:{
+                        meetupid: updMeetup.id,
+                        word: data.keywords[item].word
+                    }
+                })
+            }
+        }
+    }
+
+    async deleteMeetup(name){
+        await prisma.meetups.delete({
+            where:{ name }
+        })
+    }
 }
